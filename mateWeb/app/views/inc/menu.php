c02534906acf915394f685c3aa35c08fd59b6d4a
juanPabloCesarini <juanpcesarini@hotmail.com>
2021-02-22 19:07:58 -0300

    diseño preliminar de la home

---
+++ mateWeb/app/views/inc/menu.php
@@ -0,0 +1,63 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-matelab">
+            <img src="img/isomatelab.ico" width="40" height="40" class="d-inline-block align-top" alt="">
+                <strong>MATELAB Software Factory</strong>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse text-right" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                      <a class="nav-link" href="#"><span class="icon-home"></span>Home</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#"><span class="icon-eye"></span>Conocenos</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#"><span class="icon-cog"></span>Nuestros Servicios</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#"><span class="icon-terminal">Nuestras Tecnocnogías</span></a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#"><span class="icon-suitcase">Nuestros Clientes</span></a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#"><span class="icon-briefcase">Trabajá en MATELAB</span></a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#"><span class="icon-newspaper">mateBlog</span></a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#"><span class="icon-envelop">Contactanos</span></a>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <a class="nav-link text-white" href="#"><span class="icon-user">Mi Matelab</span></a>
+                <a class="nav-link text-white" href="#"><span class="icon-user-plus">Soy Nuevo</span></a>
+                <a class="nav-link text-white" href="#"><span class="icon-smile">Ingreso Matelabers</span></a>
+                <a class="nav-link text-white" href="#"><span class="icon-exit">Salir</span></a>
+            </div>
+        </nav>
+        <div class="social">   
+            <ul>
+                <li>
+                  <a href="#"><span class="icon-facebook2"></span></a>
+                </li>
+                <li>
+                  <a href="#"><span class="icon-instagram"></span></a>
+                </li>
+                <li>
+                  <a href="#"><span class="icon-twitter"></span></a>
+                </li>
+                <li>
+                  <a href="#"><span class="icon-github"></span></a>
+                </li>
+                <li>
+                  <a href="#"><span class="icon-linkedin"></span></a>
+                </li>
+                <li>
+                  <a href="#"><span class="icon-whatsapp"></span></a>
+                </li>
+            </ul>
+        </div>
--- mateWeb/app/views/pages/inicioView.php
+++ mateWeb/app/views/pages/inicioView.php
@@ -1,3 +1,19 @@
 <?php require RUTA_APP .'/views/inc/header_ppal.php';?>
-
+<?php require RUTA_APP .'/views/inc/menu.php';?>
+<section class="header_ppal">
+    <div class="transparencia text-dark py-5">
+        <div class="row">
+            <div class="col-sm-12 col-md-6 text-center justify-content-center align-self-center">
+                <a href="php/trabajos.php" class="btn btn-lg btn-outline-light">
+                        <h1>Solicitá tu presupuesto</h1>
+                        <h3>Sin Cargo</h3>
+                </a>
+            </div>
+            <div class="col-sm-12 col-md-6">
+                <img class="img-fluid logo d-none d-sm-block" src="img/ISO_MATELAB.png" alt="">
+            </div> 
+        </div>
+    </div>
+</section>
+<?php require RUTA_APP .'/views/inc/pie.php';?>
 <?php require RUTA_APP .'/views/inc/footer_ppal.php';?>
