40d3975a04037f1a6b442959ca1ac8495e6b32e3
Juan P. Romano <programming@jpromano.net>
2021-04-07 15:43:48 -0300

    Post Slider

--- mateBlog/js/script.js
+++ mateBlog/js/script.js
@@ -2,7 +2,15 @@
 
 $(document).ready(function(){
     $('.menu-toggle').on('click', function(){
-        $('.nav').toggleClass('showing')
-        $('.nav ul').toggleClass('showing')
-    })
-})
+        $('.nav').toggleClass('showing');
+        $('.nav ul').toggleClass('showing');
+    });
+
+    $('.autoplay').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+
+});
