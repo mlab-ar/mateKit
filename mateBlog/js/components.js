3cde38bf8de63d5c3d55beb47a4e81ac4fd672f6
Juan P. Romano <programming@jpromano.net>
2021-04-30 09:11:56 -0300

    Blog post

---
+++ mateBlog/js/components.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+    $("#incluirHeader").load('../components/header.html')
+    $("#incluirFooter").load('./components/footer.html')
+    $("#incluirNavBar").load('./components/navbar.html')
+   
+  })
